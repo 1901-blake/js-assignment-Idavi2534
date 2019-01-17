b3328cf3d944fb354dfb20ad73b6903338abbe9a
Idavi2534 <imandavis@gmail.com>
2019-01-17 14:01:00 -0500

    added to Idavi2534

--- problems/10-object-creation/10-object-creation.js
+++ problems/10-object-creation/10-object-creation.js
@@ -1,3 +1,22 @@
 /* 10 Object Creation
 Create an object using an object literal, constructor function, and es6 class.
 The object should have the fields name and age.*/
+
+
+class Person{
+
+     
+    
+ constructor(name="", age=0){
+     this.name=name;
+     this.age=age;
+    
+ }
+}
+
+let studentGirl= new Person("Iman", 23);
+
+console.log(studentGirl);
+
+
+
