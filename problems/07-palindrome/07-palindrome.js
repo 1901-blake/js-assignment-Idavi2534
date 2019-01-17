287cc0da86d834d3710d55169f4ca09ea3e1386d
Idavi2534 <imandavis@gmail.com>
2019-01-17 13:04:33 -0500

    added to Idavi2534

--- problems/07-palindrome/07-palindrome.js
+++ problems/07-palindrome/07-palindrome.js
@@ -1,6 +1,25 @@
 /* 7. Palindrome
 Define function isPalindrome(someStr)
 Return true if someStr is a palindrome, otherwise return false */
+
+
 function isPalindrome(someStr) {
-  
+
+
+
+
+let stringLength= someStr.length;
+let a= Math.floor(stringLength/2);
+
+for (let i=0; i < a; i++){
+
+    if(someStr[i] !== someStr[stringLength-1-i]){
+        return false;
+    }
 }
+
+return true;
+
+}
+
+//isPalindrome('eye');
