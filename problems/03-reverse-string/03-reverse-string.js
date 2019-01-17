7a5097429518443631337ddfd4567aea7ceff27f
Idavi2534 <imandavis@gmail.com>
2019-01-17 13:24:19 -0500

    added to Idavi2534

--- problems/03-reverse-string/03-reverse-string.js
+++ problems/03-reverse-string/03-reverse-string.js
@@ -1,6 +1,22 @@
 /* 3. Reverse String
 Define function: reverseStr(someStr)
 Reverse and return the String. */
+
 function reverseStr(someStr) {
-  
-}
+    
+
+return someStr.split("").reverse().join("");
+
+
+  /* let reversedString="";
+
+   for (let i=someStr.length -1;i >=0;i--){
+    reversedString +=someStr[i];
+    */      
+}  
+
+//}
+console.log(reverseStr("pirate"));
+
+
+
